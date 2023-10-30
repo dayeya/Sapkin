bcbef973ef19ce4c90281cda40af460acc9fb2eb
Sqrtx2 <Jonathan.Tyomkin@gmail.com>
2023-10-30 21:09:39 +0200

    Added protocol and "get all IP" command

--- Protocol/docs.py
+++ Protocol/docs.py
@@ -2,34 +2,35 @@ import pickle
 
 
 class Document:
-    
-    def __init__(self, req_type: str) -> None:
+
+    def init(self, req_type: str, payload: list=None) -> None:
         """
+
         Creates a Doc object.
 
         Args:
             req_type (str): request type for server.
         """
-        self.doc = { "type": req_type }
-        
+        self.doc = {"type": req_type, **({"payload": payload} if payload else {})}
+
     def serialize(self) -> bytes:
         """
-        Returns serialized representation of the document.
+        Returns serialized represenation of the document.
 
         Returns:
-            bytes: serialized 'seld.doc'
+            bytes: serialized 'self.doc'
         """
         return pickle.dumps(
-            self.doc
+            self
         )
-        
-    def serialize(self) -> bytes:
+
+    def deserialize(self) -> bytes:
         """
-        Returns serialized representation of the document.
+        Returns serialized represenation of the document.
 
         Returns:
-            bytes: serialized 'seld.doc'
+            bytes: deserialized 'self.doc'
         """
         return pickle.loads(
-            self.doc
+            self
         )
