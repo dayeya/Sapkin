02e2af3d444d0ed8bc96f781fa4628d994128dce
dayeya <clipchamp.daniel@gmail.com>
2023-10-31 04:45:04 +0200

    Added functionality

--- Protocol/docs.py
+++ Protocol/docs.py
@@ -5,15 +5,29 @@ class Document:
     TYPE = 'rtype'
     PAYLOAD = 'payload'
     
-    def __init__(self, req_type: str, payload: str=None) -> None:
+    def __init__(self, req_type: str, payload: list=None) -> None:
         """
         Creates a doc object.
 
         Args:
             req_type (str): request type for server.
         """
-        self.doc = {"rtype": req_type, **({"payload": payload} if payload else {})}
-        
+        self.type = {"rtype": req_type, **({"payload": payload} if payload else {})}
+    
+    def get_type(self) -> str:
+        """
+        Returns:
+            str: selfs type.
+        """
+        return self.type
+    
+    def get_payload(self) -> list:
+        """
+        Returns:
+            str: selfs payload if there exists.
+        """
+        return self.payload
+    
     def serialize(self) -> bytes:
         """
         Serializes 'self'.
@@ -30,4 +44,4 @@ class Document:
         Returns:
             str: represenation of the document.
         """
-        return f'Document: type = {self.doc[Document.TYPE]}\n' + f'paylod = {self.doc[Document.PAYLOAD]}'
+        return f'Document: type = {self.get_type()}\n' + f'paylod = {self.get_payload()}'
