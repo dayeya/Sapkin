2fa80fa5168ff822918eba65c4b8735e6de76dfc
Sqrtx2 <Jonathan.Tyomkin@gmail.com>
2023-11-03 16:31:56 +0200

    Sniffer class added + sniffing function

--- fingerprint/finger_printer.py
+++ fingerprint/finger_printer.py
@@ -1,1 +1,27 @@
 import pydivert
+
+class Sniffer:
+
+    """
+    the sniffer catches the packets that are transmitted during the communication process
+    """
+
+    def __init__(self, f: str = None) -> None:
+        """
+
+        Args:
+            f: filter for the sniffer
+        """
+        if f is not None:
+            self.w = pydivert.WinDivert(f)
+        else:
+            self.w = pydivert.WinDivert()
+
+    def sniff_packets(self):
+        print("Sniffing started")
+        with self.w as w:
+            for packet in w:
+                print(packet)
+
+
+
