772cab69ec3995f8e6080f8b087ba866b6d0c3bf
dayeya <clipchamp.daniel@gmail.com>
2023-11-03 16:55:01 +0200

    Better database arrangement

--- database/README.md
+++ database/README.md
@@ -1,19 +1,22 @@
 # Database
 
-A Database of JSON files for all OS info will describe the signatures of every OS and its versions.
-There are several types of signatures, TCP sig, MTU sig, and HTTP sig. Each JSON file will include every known signature of every OS there is.
+A database of json files for all os info will describe the signatures of every os and its versions.
+There are several types of signatures, each json file will include signatures of every os we configured.
 
 The following format will be used to all OS types
 ``` json
 {
+  /* tcp.json file */
   "os-type": {
-    "tcp":  ["signature-list"],
+    "tcp-sig":  ["signature-list"],
   },
 
+  /* mtu.json file */  
   "link-type": {
     "mtu": ["signature-list"],
   },
-
+  
+  /* http.json */
   "web-server": {
     "http": ["signature-list"]
   },
--- fingerprint/sniff.py
+++ fingerprint/sniff.py
@@ -1,27 +1,24 @@
 import pydivert
 
 class Sniffer:
-
-    """
-    the sniffer catches the packets that are transmitted during the communication process
-    """
-
-    def __init__(self, f: str = None) -> None:
+    
+    def __init__(self, filtr: str="true") -> None:
         """
         Sniffer Object
 
         Args:
             f (str, optional): Filter for the WinDivert. Defaults to None.
         """
-        if f is not None:
-            self._w = pydivert.WinDivert(f)
-        else:
-            self._w = pydivert.WinDivert()
+        self._w = pydivert.WinDivert(filtr)
 
-    def sniff_packets(self):
-        print("Sniffing started")
-        with self._w as w:
-            for packet in w:
+    def sniff_packets(self) -> None:
+        """
+        Sniffs packets.
+        """
+        
+        print("Sniffing...")
+        with self._w as packet_driver:
+            for packet in packet_driver:
                 print(packet)
 
 
