c73e29839661dad123b46309a9351ede592eadb2
Daniel <129618322+dayeya@users.noreply.github.com>
2023-12-26 13:29:49 +0200

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -1,24 +1,13 @@
 from module import Module
 
 class Server:
-    
     def __init__(self) -> None:
-        """
-        Creates Server object.
-        """
         self.server_handler = Module()
         
     def run(self) -> None:
-        """
-        Starts the server.
-        """
         self.server_handler.start()
 
     def close_server(self) -> None:
-        """
-        Closes the server.
-        :return: None
-        """
         self.server_handler.close()
 
 if __name__ == "__main__":
